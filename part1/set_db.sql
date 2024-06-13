61b5743fc508b1875fe8042816412f31e685a518
Shuoyuan Zhang <shuoyuanz@student.unimelb.edu.au>
2024-06-13 22:39:16 +1000

    Change table name and field name

--- part1/set_db.sql
+++ part1/set_db.sql
@@ -1,37 +1,37 @@
-CREATE DATABASE MindAtlas;
+CREATE DATABASE mindatlas;
 
-USE MindAtlas;
+USE mindatlas;
 
-CREATE TABLE User (
-    ID INT PRIMARY KEY,
-    Username VARCHAR(50)
+CREATE TABLE user (
+    id INT PRIMARY KEY,
+    username VARCHAR(50)
 );
 
-CREATE TABLE UserFieldName (
-    ID INT PRIMARY KEY,
-    Field VARCHAR(50)
+CREATE TABLE user_filed (
+    id INT PRIMARY KEY,
+    field VARCHAR(50)
 );
 
-CREATE TABLE UserData (
-    ID INT PRIMARY KEY,
-    FieldID INT,
-    Data VARCHAR(100),
-    UserID INT,
-    FOREIGN KEY (FieldID) REFERENCES UserFieldName(ID),
-    FOREIGN KEY (UserID) REFERENCES User(ID)
+CREATE TABLE user_data (
+    id INT PRIMARY KEY,
+    field_id INT,
+    field_data VARCHAR(100),
+    user_id INT,
+    FOREIGN KEY (field_id) REFERENCES user_filed(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-INSERT INTO User (ID, Username) VALUES 
+INSERT INTO user (id, username) VALUES
 (1, 'User1'),
 (2, 'User2'),
 (3, 'User3');
 
-INSERT INTO UserFieldName (ID, Field) VALUES 
+INSERT INTO user_filed (id, field) VALUES
 (1, 'Phone'),
 (2, 'Email'),
 (3, 'Position');
 
-INSERT INTO UserData (ID, FieldID, Data, UserID) VALUES 
+INSERT INTO user_data (id, field_id, field_data, user_id) VALUES
 (1, 1, '1111111', 1),
 (2, 2, 'User1@gmail.com', 1),
 (3, 1, '2222222', 2),
